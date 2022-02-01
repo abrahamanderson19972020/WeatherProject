ac8fe1df6eacb8ea81dedb9d22f65afae5675dd5
Abraham Anderson <abrahama@uia.no>
2022-02-01 20:20:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  console.log("Post received");
+  console.log(req.body.cityName);
+  const query = req.body.cityName;
+  url =
+    "https://api.openweathermap.org/data/2.5/weather?appid=9ea619f16c6290bbdcd480bee7e0646f&q=" +
+    query +
+    "&units=metric";
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      console.log(weatherData);
+      const temp = weatherData.main.temp;
+      console.log(temp);
+      const description = weatherData.weather[0].description;
+      console.log(description);
+      const icon = weatherData.weather[0].icon;
+      console.log(icon);
+      const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<h1> Weather Conditions </h1>");
+      res.write("<p>It is " + temp + " degrees </p>");
+      res.write("<img src=" + imgURL + ">");
+      res.send();
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
